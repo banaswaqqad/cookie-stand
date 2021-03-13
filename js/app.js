8a56b7fef4daf53f02730a32bdc6885b9a4e4c24
banaswaqqad <Banawaqqad7@gmail.com>
2021-03-13 10:24:46 +0200

    will work on it later

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 'use strict '
 
+
+let myform = document.getElementById('msgform')
+myform.addEventListener('submit',myfunction)
+function myfunction(even)
+{}
+
+
 let table = document.getElementById('mytable')
 let workingHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm' ];
 
